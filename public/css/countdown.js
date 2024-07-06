476eb1f1a3b9bd83ef08f248f6cbb123c89103d1
FamiliaEn <102762992+FamiliaEn@users.noreply.github.com>
2024-07-05 22:59:20 -0600

    relog
    relog

--- public/css/countdown.js
+++ public/css/countdown.js
@@ -3,7 +3,7 @@ simplyCountdown('#cuenta', {
 	month: 07, // required
 	day: 14, // required
 	hours: 13, // Default is 0 [0-23] integer
-	minutes: 00, // Default is 0 [0-59] integer
+	minutes: 01, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
 		days: 'Día',
